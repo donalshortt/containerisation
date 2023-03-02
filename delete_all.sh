3c560a36a1862d7d8b9cc38d64a522fd656c75b0
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2023-03-02 13:39:34 +0100

    Delete script I accidentally added previously

--- delete_all.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete deployments --all
-kubectl delete services --all
-kubectl delete pods --all
-kubectl delete daemonset --all
-
-echo "do minikube delete if you really want to reset everything as much as you can"
